03bdd3403714d83db7cc9e0381b48ef3518dce80
karmael161 <ekarmael@gmail.com>
2024-09-19 14:22:04 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,8 +12,8 @@ global.location = "Port Harcourt, Nigeria" //ur location
 
 //new
 global.botname = 'ELITE-PRO-V2' //ur bot name
-global.ownernumber = '2348109263390' //ur owner number
-global.ownername = 'ELITE-PRO-V2' //ur owner name
+global.ownernumber = '237672080608' //ur owner number
+global.ownername = '𓆩ᬼ⃟𝗚𝗢𝗗⛃⃟༄𝐌𝐈𝐆𝐇𝐓𝐘᭄亗𝐒𝐀𝐒𝐀𝐊𝐈亗࿐' //ur owner name
 global.websitex = "https://youtu.be/@elite-pro-empire"
 global.wagc = "https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g"
 global.themeemoji = '👨🏼‍💻'
@@ -22,8 +22,8 @@ global.botscript = 'https://github.com/Elite-MD/ELITE-PRO-V2' //script link
 global.packname = "Sticker By"
 global.author = "ELITE-PRO-V2"
 global.creator = "2348109263390@s.whatsapp.net"
-global.xprefix = '.'
-global.premium = ["2348109263390"] // Premium User
+global.xprefix = '✓'
+global.premium = ["237672080608"] // Premium User
 global.hituet = 0
 
 //bot sett
